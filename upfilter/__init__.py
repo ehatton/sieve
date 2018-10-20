cff180a1ecb9b61aadae7fd7d58aee293591e827
Emma Hatton-Ellis <ehattonellis@gmail.com>
2018-10-20 16:32:13 +0100

    Adding docstring

--- upfilter/__init__.py
+++ upfilter/__init__.py
@@ -1,2 +1,5 @@
+"""Tools for reading and filtering UniProt fasta sequences."""
+__version__ = '0.1'
+
 # Give access to the FastaParser class from the top level of the package
 from upfilter.fasta_parser import FastaParser
