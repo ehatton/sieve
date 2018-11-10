647ae039eeb8667403cd2b48a1d761ec924a2b81
Emma Hatton-Ellis <ehattonellis@gmail.com>
2018-11-10 16:53:56 +0000

    Changing version

--- upfilter/__init__.py
+++ upfilter/__init__.py
@@ -1,5 +1,5 @@
 """Tools for reading and filtering UniProt fasta sequences."""
-__version__ = '0.1'
+__version__ = '0.0.0'
 
 # Give access to the Fasta and FastaParser classes from the top level of the package
 from upfilter.fasta_parser import Fasta, FastaParser
