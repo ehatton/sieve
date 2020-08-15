ec7a8836cd333d7d3b545e4216eedf599a2259c7
Emma Hatton-Ellis <ehattonellis@gmail.com>
2020-08-15 17:39:49 +0100

    Moving version to separate file

--- sieve/__init__.py
+++ sieve/__init__.py
@@ -1,7 +1,5 @@
 """Tools for reading and filtering UniProt fasta sequences."""
-__version__ = "1.0.0"
 
-# Give access to the Fasta and FastaParser classes from the top level of the package
 from .fasta import Fasta
-from .fasta_parser import FastaParser
-from .text_parser import text_parser
+from .fasta_parser import parse_fasta
+from .text_parser import parse_text
